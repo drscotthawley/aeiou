eec11a4e3674564999b3a5b39d083d7c3f06bc7f
Scott H. Hawley <scott.hawley@belmont.edu>
2022-08-15 23:46:43 -0500

    added docments for HostPrinter

--- aeiou/hpc.py
+++ aeiou/hpc.py
@@ -33,7 +33,12 @@ def get_accel_config(filename='~/.cache/huggingface/accelerate/default_config.ya
 # %% ../05_hpc.ipynb 9
 class HostPrinter():
     "lil accelerate utility for only printing on host node"
-    def __init__(self, accelerator, tag='\033[96m', untag='\033[0m'): #added some colors
+    def __init__(
+        self, 
+        accelerator,    # huggingface accelerator object
+        tag='\033[96m', # starting color
+        untag='\033[0m' # reset to default color
+    ): 
         self.accelerator, self.tag, self.untag = accelerator, tag, untag
     def __call__(self, s:str):
         if self.accelerator.is_main_process:
